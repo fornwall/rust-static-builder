8895fc318289d63cbd096aa86fa880e414b109f4
Fredrik Fornwall <fredrik@fornwall.net>
2018-12-01 15:56:57 +0100

    Use strip -s, not strip -x

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e -u
 cargo build --release --target x86_64-unknown-linux-musl
 
 if [ -z ${NOSTRIP+x} ]; then
-	strip -x `find target/x86_64-unknown-linux-musl/release/ -executable -type f -maxdepth 1`
+	strip -s `find target/x86_64-unknown-linux-musl/release/ -executable -type f -maxdepth 1`
 else
 	echo "Not stripping"
 fi
